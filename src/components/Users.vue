149d3f1a9b379a19b7fe0f29b5ef99eda79f84c2
Hector Lopez <hectorlopeztrejo9@gmail.com>
2020-12-26 14:21:44 -0700

    Added User component

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -2,3 +2,21 @@
     <h3>Users:</h3>
 </template>
 
+<script>
+export default {
+    name: 'Users',
+    data: {
+        'content': content
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style>
+    h3 {
+        margin-bottom: 5%;
+        margin-top: auto;
+    }
+</style>
